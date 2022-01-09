47d64a9c1898762fe362cdb8d779cc84d22750b1
Dan Adeholt <dan@binarypeak.se>
2022-01-09 21:18:50 +0100

    Simplified 17 even more

--- 17.ts
+++ 17.ts
@@ -1,4 +1,4 @@
-import { maxBy, parseInt } from 'lodash-es';
+import { parseInt, flatMap } from 'lodash-es';
 import { readLines } from './loadData';
 
 const triangularNumber = (n) => (n * (n + 1)) / 2;
@@ -8,57 +8,48 @@ const xPosAtIteration = (velX, iteration) => {
   return velX * cappedIteration - triangularNumber(cappedIteration - 1);
 };
 
-type VerticalHit = {
-  initialVelocity: number,
-  peakY: number,
-  iteration: number
-};
-
 export const calculateHits = (returnTotal: boolean) => {
-  const line = readLines('17.txt')[0];
-  const [xRange, yRange] = line
+  const range = flatMap(readLines('17.txt')[0]
     .split('x=')[1]
     .split(', y=')
-    .map((range) => range
+    .map((innerRange) => innerRange
       .split('..')
-      .map(parseInt));
+      .map(parseInt)));
 
-  const [minX, maxX] = xRange;
-  const [minY, maxY] = yRange;
-
-  const yHits: VerticalHit[] = [];
+  const [minX, maxX, minY, maxY] = range;
+  let totalMaxPeakY = 0;
+  let hitCount = 0;
 
   // Find all y velocities that overlap with the box
   // Every initialVelY > 0 will arrive back at y = 0 with velY = -(initialVelY + 1), so no velocity
   // larger than -minY - 1 has a chance of hitting the box since they will overshoot it.
+  const compatibleXVelocities: boolean[] = new Array(maxX + 1);
+
   for (let velY = -minY - 1; velY >= minY; velY--) {
+    compatibleXVelocities.fill(false);
     for (let iteration = 0, curVel = velY, posY = 0, peakY = 0; posY >= minY; iteration++) {
       if (posY >= minY && posY <= maxY) {
-        yHits.push({ initialVelocity: velY, iteration, peakY });
+        totalMaxPeakY = Math.max(totalMaxPeakY, peakY);
+
+        for (let velX = 1; velX <= maxX; velX++) {
+          const potentialXpos = xPosAtIteration(velX, iteration);
+          if (potentialXpos >= minX && potentialXpos <= maxX && !compatibleXVelocities[velX]) {
+            compatibleXVelocities[velX] = true;
+            hitCount++;
+          }
+        }
       }
 
       peakY = Math.max(peakY, posY);
-      posY += curVel;
-      curVel--;
+      posY += curVel--;
     }
   }
 
   if (returnTotal) {
-    const hits = new Set<string>();
-    // Go through all Y hits, find all X velocities that would also hit the box at the same time.
-    for (const yHit of yHits) {
-      for (let velX = 1; velX <= maxX; velX++) {
-        const potentialXpos = xPosAtIteration(velX, yHit.iteration);
-        if (potentialXpos >= minX && potentialXpos <= maxX) {
-          hits.add(`${velX},${yHit.initialVelocity}`);
-        }
-      }
-    }
-
-    return hits.size;
+    return hitCount;
   }
 
-  return maxBy(yHits, 'peakY').peakY;
+  return totalMaxPeakY;
 };
 
 export const problem1 = () => calculateHits(false);
